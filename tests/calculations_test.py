41ce89ae5179c4eced17e6debadcfda5006770f4
Phil Evans <philenz@gmail.com>
2025-10-19 23:23:06 +0000

    more tests please

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,3 +68,11 @@ def test_get_nth_fibonacci_ten():
 
     # Assert
     assert result == 55
+
+def test_area_of_circle_negative():
+    with pytest.raises(ValueError):
+        area_of_circle(-1)
+
+def test_get_nth_fibonacci_negative():
+    with pytest.raises(ValueError):
+        get_nth_fibonacci(-5)
